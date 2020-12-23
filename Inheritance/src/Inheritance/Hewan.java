720d115a54e00ffa5e0c83b2f97ed65c70dab521
Dianminii <74170971+Dianminii@users.noreply.github.com>
2020-12-23 12:46:33 +0700

    Add files via upload

---
+++ Inheritance/src/Inheritance/Hewan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inheritance;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Hewan {
+ public Hewan() {
+        System.out.println("construct Hewan");
+    }
+ public void makan() {
+        System.out.println("Hewan sedang makan..");
+    }
+}
